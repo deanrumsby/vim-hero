9fa46144ccd5b8fbacdb92ed1fdb9af2fcdb78c0
Dean Rumsby <dean@deanrumsby.com>
2023-04-08 15:49:05 +0100

    Add required headers for sharedArrayBuffer access

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
+const allowSharedBufferHeaders = [
+  { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+  { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+];
+
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: [
+    'vim-wasm',
+    'react-vim-wasm',
+  ],
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: allowSharedBufferHeaders,
+      }
+    ];
+  },
 }
 
 module.exports = nextConfig
